467353f9e5afe47b1122748f16a956692be792ca
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-04-19 22:30:57 -0400

    updated jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Deck from './components/Deck'
+interface Player {
+  hand: any,
+  hasPlayerPassed: boolean,
+  hasPlayerLoss: boolean,
+  playerName: string
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+}
+class Player {
+  constructor () {
+    this.hand = []
+    this.hasPlayerPassed = false
+    this.hasPlayerLoss = false
+    this.playerName = ''
+  }
+
+  addCard (getCard: string) {
+
+  }
+
+ getScore () {
+
+ }
+
+}
+class App extends React.Component {
+  baseState = {
+    deck: new Deck();
+    players: [new Player (), new Player ()],
+    playerTurn: 0,
+    displayMessage: "Hey Let's Play Black Jack",
+    gameOver: false
+
+  }
+  constructor () {
+    super()
+this.state = {
+    ...this.baseState, players: [ new Player(), new Player ()]
+  }
+}
+}
+
+render () {
+  return <div> 
+
+    <div> display Message : {this.state.displayMessage}</div>
+  </div>
 }
 
 export default App;
